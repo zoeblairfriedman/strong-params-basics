c731f5dab4e4e08b6de51d994ac46b14c4ecd473
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-10 18:08:16 -0500

    commit tree 1c24baf8ccf36aa1d718872b97e7cd677b3e1915

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,19 +11,28 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
-
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
-
 	def edit
-	  @post = Post.find(params[:id])
-	end
+		@post = Post.find(params[:id])
+	  end
+
+	def create
+		@post = Post.new(post_params(:title, :description))
+		@post.save
+		redirect_to post_path(@post)
+	  end
+	  
+	  def update
+		@post = Post.find(params[:id])
+		@post.update(post_params(:title))
+		redirect_to post_path(@post)
+	  end
+	  
+	  private
+	  
+	
+	  def post_params(*args)
+		params.require(:post).permit(*args)
+	  end
+
+
 end
